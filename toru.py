ccbb1dd54a62a782ffadfa4595fade282860c571
Puh00 <alex.phu@live.com>
2021-06-23 16:18:30 +0200

    gifs

--- toru.py
+++ toru.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 
+from toru.giphy import get_gif, cat_gif
+
 load_dotenv()
 
 client = commands.Bot(command_prefix="!")
@@ -31,6 +33,16 @@ async def on_message(message: discord.Message):
 @client.command()
 async def ping(ctx: Context):
     await ctx.send("pong!")
+
+
+@client.command()
+async def gif(ctx: Context, arg: str = ""):
+    await ctx.send(get_gif(arg))
+
+
+@client.command()
+async def cat(ctx: Context):
+    await ctx.send(cat_gif())
 
 
 client.run(os.getenv("DISCORD_TOKEN"))
